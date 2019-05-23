afe0ddc690b647fe411a696b629dc3c7486e94e5
misterRogers <c.a.r0g3r5@gmail.com>
2019-05-22 22:08:22 -0700

    working here

--- vertical/src/components/goals/goalsForm.js
+++ vertical/src/components/goals/goalsForm.js
@@ -12,17 +12,35 @@ class GoalsForm
 
   addGoal = event => {
     event.preventDefault();
-    axiosWithAuth() 
-    .post('https://awsafran-vertical.herokuapp.com/goals/3', {"goalvertical":  this.state.goal})
-    .then(res => {
-      console.log(res.data);
-      this.setState({
-        goal: ''
-      });
-    })
-    .catch((err) => console.log(err));
+    // axiosWithAuth() 
+    // .post('https://awsafran-vertical.herokuapp.com/goals/3', {"goalvertical":  this.state.goal})
+    // .then(res => {
+    //   console.log(res.data);
+    //   this.setState({
+    //     goal: ''
+    //   });
+    // })
+    // .catch((err) => console.log(err));
     //window.location.reload();
+
+    fetch("https://awsafran-vertical.herokuapp.com/goals/3", {
+		body: `{"goalvertical" : ${this.state.goal}}`,
+		headers: {
+			Authorization: `bearer ${localStorage.getItem('token')}`,
+			"Content-Type": "application/json"
+		},
+		method: "POST"
+		}).then(res => res.json()).then(res =>{
+		// localStorage.setItem('token', res.access_token);
+		// this.setState({login: true});
+		console.log(res);
+		})
+		.catch( err => {
+			console.log(err);
+		});
   };
+
+
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
